65662eb8e6c15748ef5be40d71386c7051901b6d
AnandDL02 <ananddalsaniya2002@gmail.com>
2024-05-24 13:58:09 +0530

    all

---
+++ day-52/file.js
@@ -0,0 +1,18 @@
+function nav() {
+   
+    const navbar =`
+    <center>
+    <nav>
+    <a href="">home</a>
+    <a href="">about</a>
+    <a href="">contact</a>
+    <a href="">cart</a>
+</nav>
+</center>
+`
+
+document.getElementById("navbar").innerHTML=navbar
+
+}
+
+export {nav}
